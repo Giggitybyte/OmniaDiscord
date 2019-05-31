0e1101fe8572b8268cb4bd6ebf321dfb95bd9ea9
Luke Triano <luketriano115@gmail.com>
2019-05-30 18:51:59 -0700

    Removed all things relating to guild logging.
    Perhaps I'll add this in later, but right now it honestly seems pointless.

--- OmniaDiscord/Entities/Database/GuildData.vb
+++ OmniaDiscord/Entities/Database/GuildData.vb
@@ -5,16 +5,13 @@
         Public Property Id As Integer ' Database ID.
         Public Property GuildId As ULong ' The guild the data belongs to.
 
-        Public Property Cases As Dictionary(Of Integer, GuildCase) ' TODO.
-        Public Property MutedMembers As List(Of ULong) ' Members who are not allowed to speak. 
-        Public Property DiscJockeys As List(Of ULong) ' Collection of users allowed to queue music.
+        Public Property MutedMembers As New List(Of ULong) ' Members who are not allowed to speak. 
+        Public Property DiscJockeys As New List(Of ULong) ' Collection of users allowed to queue music.
         Public Property StaffTitles As Dictionary(Of GuildTitle, List(Of ULong)) ' Collection of users with staff titles.
-        Public Property LobbyChannels As List(Of ULong) ' Collection of voice channels that will be treated as lobby channels.
-        Public Property GameChannels As List(Of ULong) ' Collection of voice channels that will be used in the auto move system.
+        Public Property LobbyChannels As New List(Of ULong) ' Collection of voice channels that will be treated as lobby channels.
+        Public Property GameChannels As New List(Of ULong) ' Collection of voice channels that will be used in the auto move system.
 
         Sub New()
-            _MutedMembers = New List(Of ULong)
-            _DiscJockeys = New List(Of ULong)
             _StaffTitles = New Dictionary(Of GuildTitle, List(Of ULong)) From {
                 {GuildTitle.Admin, New List(Of ULong)()},
                 {GuildTitle.Moderator, New List(Of ULong)()},
