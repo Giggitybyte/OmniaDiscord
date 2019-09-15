4bb7d0841d7e72f8d8b189ab2230dc68a8464499
Luke Triano <luketriano115@gmail.com>
2019-09-15 01:06:36 -0700

    Minor help command refactor.
    Help command now displays full child commands under usage along with arguments and overloads for each child command.

--- OmniaDiscord/Commands/HelpFormatter.vb
+++ OmniaDiscord/Commands/HelpFormatter.vb
@@ -27,35 +27,14 @@ Namespace Commands
             Dim aliases = GetCommandAliases(cmd)
             If aliases.Any Then _strBuilder.AppendLine.AppendLine("aliases::").AppendLine(String.Join(", ", aliases))
 
-            Dim usageBuilder As New StringBuilder
-            For Each overload In cmd.Overloads.OrderBy(Function(x) x.Priority)
-                If Not overload.Arguments.Any Then
-                    usageBuilder.AppendLine($"{_prefix}{cmd.QualifiedName}")
-                    Continue For
-                End If
+            Dim usages As New StringBuilder
+            usages.Append(GetCommandUsage(cmd))
 
-                usageBuilder.Append($"{_prefix}{cmd.QualifiedName} ")
-                For Each arg As CommandArgument In overload.Arguments
-                    usageBuilder.Append($"{If(arg.IsOptional, "(", "[")}{arg.Name}{If(arg.IsOptional, ")", "]")} ")
-                Next
-                usageBuilder.AppendLine()
-            Next
-            If usageBuilder.Length > 0 Then _strBuilder.AppendLine.AppendLine("usage::").Append(usageBuilder.ToString)
-
-            If TypeOf cmd Is CommandGroup Then
-                Dim cmdGroup = DirectCast(cmd, CommandGroup)
-
-                If cmdGroup.Children.Any Then
-                    _strBuilder.AppendLine.AppendLine("child commands::")
-                    _strBuilder.AppendLine(String.Join(", ", cmdGroup.Children.Select(Function(c) c.Name)))
-                End If
-
-                If cmdGroup.IsExecutableWithoutSubcommands Then
-                    _strBuilder.AppendLine.AppendLine("// this command can be executed without a child command")
-                Else
-                    _strBuilder.AppendLine.AppendLine("[this command must be executed with a child command]")
-                End If
+            If usages.Length > 0 Then
+                _strBuilder.AppendLine.AppendLine("usage::").Append(usages.ToString)
             End If
+
+            _strBuilder.AppendLine.AppendLine("// [arg] = required • (arg) = optional")
 
             Return Me
         End Function
@@ -88,6 +67,30 @@ Namespace Commands
             If Not cmd.Aliases.Any Then Return New List(Of String)
             If cmd.Parent IsNot Nothing Then Return cmd.Aliases.Select(Function(a) $"{cmd.Parent.QualifiedName} {a}")
             Return cmd.Aliases
+        End Function
+
+        Private Function GetCommandUsage(cmd As Command) As String
+            Dim usageBuilder As New StringBuilder
+            For Each overload In cmd.Overloads.OrderBy(Function(x) x.Priority)
+                If Not overload.Arguments.Any Then
+                    usageBuilder.AppendLine($"{_prefix}{cmd.QualifiedName}")
+                    Continue For
+                End If
+
+                usageBuilder.Append($"{_prefix}{cmd.QualifiedName} ")
+                For Each arg As CommandArgument In overload.Arguments
+                    usageBuilder.Append($"{If(arg.IsOptional, "(", "[")}{arg.Name}{If(arg.IsOptional, ")", "]")} ")
+                Next
+                usageBuilder.AppendLine()
+            Next
+
+            If TypeOf cmd Is CommandGroup Then
+                For Each childCmd In DirectCast(cmd, CommandGroup).Children
+                    usageBuilder.Append(GetCommandUsage(childCmd))
+                Next
+            End If
+
+            Return usageBuilder.ToString
         End Function
     End Class
 End Namespace
